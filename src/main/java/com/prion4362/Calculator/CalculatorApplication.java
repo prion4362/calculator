a70b1c7f5106eb06b66e5daa6b0a68df652edcfb
Les <prion4362@hotmail.com>
2021-09-11 16:19:45 -0400

    Add Spring Boot skeleton

---
+++ src/main/java/com/prion4362/Calculator/CalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.prion4362.Calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prion4362/Calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prion4362.Calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
